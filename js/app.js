b76d3aed8604d7abbcf85ae5dcabaf539f1e73cd
Carlos Cal <putrisimio@gmail.com>
2018-11-22 09:59:50 -0600

    datos guardados

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+comve =()=>{
+    let harina = document.getElementById("hari").value;
+    let conver = harina * 16.000*2.5/100;
+    let huevos = parseInt( harina * 16.000  * 0.2/16);
+    let bicicleta = parseInt(harina * 16.000 * 3.57/100/2);
+    document.sales.convert.value =conver;
+    document.huevoss.huev.value = huevos;
+    document.lala.ahora.value = bicicleta;
+
+
+};
